03146e13da05cbe0eab4b9d7eb4437f0758e524a
Tomis <31387372+TomNett@users.noreply.github.com>
2023-02-02 15:02:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,17 +24,3 @@ player_image_container, horizontal_bar_container = st.columns((1, 3))
 player_image_container.text(' ')
 player_image_container.text(' ')
 player_image_container.text(' ')
-
-with horizontal_bar_container:
-    st.plotly_chart(plot_horizontal_bar(epl_strikers_df, player), 
-                    use_container_width=True
-    )
-line_plot_container, scatter_plot_container = st.columns(2)
-with line_plot_container:
-    st.plotly_chart(plot_line_plot(epl_strikers_df, player),
-                    use_container_width=True
-   )
-with scatter_plot_container:
-    st.plotly_chart(plot_scatter_plot(epl_strikers_df,player),
-                  use_container_width=True
-    )
