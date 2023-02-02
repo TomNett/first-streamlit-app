315c59e2506825aba9617b6c6d6b93c2b14fdaef
Tomis <31387372+TomNett@users.noreply.github.com>
2023-02-02 14:55:26 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ left_column.text('Choose A Player for Analysis:')
 with right_column:
     player = st.selectbox('', list(epl_strikers_df['player_name'].unique()))
 # Space out the maps so the second one is 3x the size of the other three
-player_image_container, horizontal_bar_container = st.beta_columns((1, 3))
+player_image_container, horizontal_bar_container = st.columns((1, 3))
 player_image_container.text(' ')
 player_image_container.text(' ')
 player_image_container.text(' ')
@@ -31,7 +31,7 @@ with horizontal_bar_container:
     st.plotly_chart(plot_horizontal_bar(epl_strikers_df, player), 
                     use_container_width=True
     )
-line_plot_container, scatter_plot_container = st.beta_columns(2)
+line_plot_container, scatter_plot_container = st.columns(2)
 with line_plot_container:
     st.plotly_chart(plot_line_plot(epl_strikers_df, player),
                     use_container_width=True
