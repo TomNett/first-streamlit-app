e4d01cabf6d67ee7fac54b78c5691ed27048ee35
Tomis <31387372+TomNett@users.noreply.github.com>
2023-02-02 15:07:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,15 +2,15 @@ import streamlit as st
 import numpy as np 
 
 #import matplotlib
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
+#from sklearn import datasets
+#from sklearn.model_selection import train_test_split
 
-from sklearn.decomposition import PCA
-from sklearn.svm import SVC
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
+#from sklearn.decomposition import PCA
+#from sklearn.svm import SVC
+#from sklearn.neighbors import KNeighborsClassifier
+#from sklearn.ensemble import RandomForestClassifier
 
-from sklearn.metrics import accuracy_score
+#from sklearn.metrics import accuracy_score
 
 st.title('Streamlit Example')
 
